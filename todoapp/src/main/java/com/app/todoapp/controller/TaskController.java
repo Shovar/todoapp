d268cc13cd879ee48b9744361d327b7ef0f63c5f
Shovar <9300toni@gmail.com>
2025-03-09 17:46:54 +0100

    Final commit

--- todoapp/src/main/java/com/app/todoapp/controller/TaskController.java
+++ todoapp/src/main/java/com/app/todoapp/controller/TaskController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
@@ -25,4 +28,23 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "tasks";
     }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/"; // redirect to the root URL (es un refresh)
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
 }
